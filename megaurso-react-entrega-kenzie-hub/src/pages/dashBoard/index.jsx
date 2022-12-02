039cea39a590d298620508c6e45af977959b15a9
megaurso <bernadogui@hotmail.com>
2022-12-02 18:41:47 -0300

    fix: nothing

--- megaurso-react-entrega-kenzie-hub/src/pages/dashBoard/index.jsx
+++ megaurso-react-entrega-kenzie-hub/src/pages/dashBoard/index.jsx
@@ -4,58 +4,63 @@ import { Header } from "../../components/header";
 import { api } from "../../services/api";
 import { DivDashBoard } from "./style";
 
-export function DashBoard(){
-    const navigate = useNavigate()
+export function DashBoard() {
+  const navigate = useNavigate();
 
-    const [userInfo, setUserInfo] = useState("")
+  const [userInfo, setUserInfo] = useState("");
 
-    function getLocalStorage(){
-        const token = localStorage.getItem("@TOKEN") || ""
+  function getLocalStorage() {
+    const token = localStorage.getItem("@TOKEN") || "";
 
-        if(token == ""){
-            navigate("/")
-        }
-        return token
+    if (token == "") {
+      navigate("/");
     }
+    return token;
+  }
 
-    useEffect(()=>{
-        async function getApi(){
-            const token = getLocalStorage()
-            try {
-                const response = await api.get("/profile",{
-                    headers:{
-                        "Content-Type":"application/json",
-                        "Authorization": `Bearer ${token}`
-                    }
-                })
-                setUserInfo(response.data) 
-            } catch (error) {
-                console.log(error)
-            }
-        }getApi()
-    },[])
-
-    function removeLocalStorage(){
-        const remove = localStorage.removeItem("@TOKEN") && localStorage.removeItem("@USERID")
-       
-        return remove
+  useEffect(() => {
+    async function getApi() {
+      const token = getLocalStorage();
+      try {
+        const response = await api.get("/profile", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUserInfo(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     }
+    getApi();
+  }, []);
 
+  function removeLocalStorage() {
+    const remove =
+      localStorage.removeItem("@TOKEN") && localStorage.removeItem("@USERID");
 
-    return (
-        <DivDashBoard>
-        <Header>
-            <h1>Kenzie Hub</h1>
-            <Link onClick={removeLocalStorage} to={"/"}>Sair</Link>
-        </Header>
-        <section>
-            <h1>Olá, {userInfo.name}</h1>
-            <span>{userInfo.course_module}</span>
-        </section>
-        <main>
-            <h1>Que pena! Estamos em desenvolvimento :(</h1>
-            <p>Nossa aplicação está em desenvolvimento, em breve teremos novidades</p>
-        </main>
-        </DivDashBoard>
-    )
+    return remove;
+  }
+
+  return (
+    <DivDashBoard>
+      <Header>
+        <h1>Kenzie Hub</h1>
+        <Link onClick={removeLocalStorage} to={"/"}>
+          Sair
+        </Link>
+      </Header>
+      <section>
+        <h1>Olá, {userInfo.name}</h1>
+        <span>{userInfo.course_module}</span>
+      </section>
+      <main>
+        <h1>Que pena! Estamos em desenvolvimento :(</h1>
+        <p>
+          Nossa aplicação está em desenvolvimento, em breve teremos novidades
+        </p>
+      </main>
+    </DivDashBoard>
+  );
 }
--- megaurso-react-entrega-kenzie-hub/src/pages/login/index.jsx
+++ megaurso-react-entrega-kenzie-hub/src/pages/login/index.jsx
@@ -1,7 +1,7 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Link, useNavigate } from "react-router-dom";
 import { DivFormLogin } from "./style";
-import * as yup from "yup"
+import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { api } from "../../services/api";
 import { Buttons } from "../../components/buttons";
@@ -9,49 +9,63 @@ import { Inputs } from "../../components/inputs";
 import { Forms } from "../../components/form";
 import { toast } from "react-toastify";
 
+export function Login() {
+  const navigate = useNavigate();
 
-export function Login(){
-    const navigate = useNavigate()
+  const formSchema = yup.object().shape({
+    email: yup.string().required("Email Obrigatório").email(),
+    password: yup.string().required("Senha Obrigatória"),
+  });
 
-    const formSchema = yup.object().shape({
-        email: yup.string().required("Email Obrigatório").email(),
-        password: yup.string().required("Senha Obrigatória")
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
 
-    const {register , handleSubmit , formState: {errors}} = useForm({
-        resolver: yupResolver(formSchema)
-    })
-
-    async function postHandleSubmit(data) {
-        try {
-            const response = await api.post("/sessions",data)
-            toast.success("Login efetuado com sucesso!")
-            window.localStorage.clear()
-            window.localStorage.setItem("@TOKEN", response.data.token)
-            navigate("/dashboard")
-        } catch (error) {
-            console.log(error)
-            toast.error("Algo deu errado tente novamente!")
-        } finally{}
+  async function postHandleSubmit(data) {
+    try {
+      const response = await api.post("/sessions", data);
+      toast.success("Login efetuado com sucesso!");
+      window.localStorage.clear();
+      window.localStorage.setItem("@TOKEN", response.data.token);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+      toast.error("Algo deu errado tente novamente!");
+    } finally {
     }
+  }
 
-    return (
-        <DivFormLogin>
-            <h1>Kenzie Hub</h1>
-            <Forms onSubmit={handleSubmit(postHandleSubmit)}>
-                <h2>Login</h2>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <Inputs id="email" type="email" placeholder="Digite seu email" register={{...register("email")}}/>
-                    <p aria-errormessage="error">{errors.email?.message}</p>
-                    <label htmlFor="pass">Senha</label>
-                    <Inputs id="pass" type="password" placeholder="Digite sua senha"  register={{...register("password")}}/>
-                    <p aria-errormessage="error">{errors.password?.message}</p>
-                </div>
-                <Buttons type="submit">Entrar</Buttons>
-                <span>Ainda não possui uma conta?</span>
-                <Link to={"/register"}>Cadastre-se</Link>
-            </Forms>
-        </DivFormLogin>
-    )
+  return (
+    <DivFormLogin>
+      <h1>Kenzie Hub</h1>
+      <Forms onSubmit={handleSubmit(postHandleSubmit)}>
+        <h2>Login</h2>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Inputs
+            id="email"
+            type="email"
+            placeholder="Digite seu email"
+            register={{ ...register("email") }}
+          />
+          <p aria-errormessage="error">{errors.email?.message}</p>
+          <label htmlFor="pass">Senha</label>
+          <Inputs
+            id="pass"
+            type="password"
+            placeholder="Digite sua senha"
+            register={{ ...register("password") }}
+          />
+          <p aria-errormessage="error">{errors.password?.message}</p>
+        </div>
+        <Buttons type="submit">Entrar</Buttons>
+        <span>Ainda não possui uma conta?</span>
+        <Link to={"/register"}>Cadastre-se</Link>
+      </Forms>
+    </DivFormLogin>
+  );
 }
